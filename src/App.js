ed0a1dd6be139badf983f378172a53eccb2108fa
CristianAlarconM <c.alarconmoraga@gmail.com>
2022-09-26 21:56:00 -0300

    modificaciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState } from 'react'
+import { baseColaboradores } from './baseColaboradores';
 function App() {
+
+  const [nuevoColaborador, setNuevoColaborador] = useState({ nombre: "", correo: "" });
+  const [listadoColaboradores, setListadoColaboradores] = useState(baseColaboradores)
+
+  const capturaInput = (e, propiedad) => {
+    setNuevoColaborador({ ...nuevoColaborador, [propiedad]: e.target.value })
+    console.log(nuevoColaborador)
+
+  }
+  const envioDatos = () => {
+    setListadoColaboradores([...listadoColaboradores, { ...nuevoColaborador, id: Date.now() }])
+  }
+  const [buscadorColaboradores, setBuscadorColaboradores] = useState("");
+
+  const buscadorColaborador = (e) => {
+    setBuscadorColaboradores(e.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div>
+        <div className='buscador'><h2>Buscador de colaboradores</h2><input onChange={buscadorColaborador}></input><br></br><br></br></div>
+        <div className='contenedor'><h2>Agregar Colaboradores</h2><label> Nombre del colaborador </label>
+          <input name="name" placeholder='Ingrese el nombre' onChange={(ev) => {
+            capturaInput(ev, "nombre")
+          }} ></input>
+          <label> Correo del colaborador </label>
+          <input type="email" name="correo" placeholder='Ingrese el correo' onChange={(ev) => { capturaInput(ev, "correo") }} />
+          <br></br><br></br><button onClick={envioDatos}>Agregar colaborador</button></div>
+
+        <ul>
+          <h2>Listado de Colaboradores</h2>
+          {listadoColaboradores.filter((colaborador) => {
+            return (
+              colaborador.nombre.toLowerCase().includes(buscadorColaboradores.toLocaleLowerCase()))
+          }).map(colaborador => { return < li key={colaborador.id} > nombre: {colaborador.nombre}- correo: {colaborador.correo}</li> })}
+        </ul></div >
+
     </div>
   );
 }
